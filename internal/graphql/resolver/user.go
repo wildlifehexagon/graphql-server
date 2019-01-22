686f01c0ea508b4de342ed859fd62e8c6be62ba8
wildlifehexagon <crossthebreeze@gmail.com>
2019-01-22 16:18:10 -0600

    feat: add UserByEmail resolver and update CreateUser

--- internal/graphql/resolver/user.go
+++ internal/graphql/resolver/user.go
@@ -26,6 +26,30 @@ func (r *Resolver) Query() generated.QueryResolver {
 type mutationResolver struct{ *Resolver }
 
 func (r *mutationResolver) CreateUser(ctx context.Context, input *models.CreateUserInput) (*models.User, error) {
+	_, err := r.UserClient.CreateUser(context.Background(), &user.CreateUserRequest{
+		Data: &user.CreateUserRequest_Data{
+			Type: "user",
+			Attributes: &user.UserAttributes{
+				FirstName: input.FirstName,
+				LastName:  input.LastName,
+				Email:     input.Email,
+				// Organization:  input.Organization,
+				// GroupName:     input.GroupName,
+				// FirstAddress:  input.FirstAddress,
+				// SecondAddress: input.SecondAddress,
+				// City:          input.City,
+				// State:         input.State,
+				// Zipcode:       input.Zipcode,
+				// Country:       input.Country,
+				// Phone:         input.Phone,
+				IsActive: input.IsActive,
+			},
+		},
+	})
+	if err != nil {
+		r.Logger.Errorf("Error creating new user from mutation resolver: %s", err)
+		return nil, err
+	}
 	user := &models.User{
 		FirstName:     input.FirstName,
 		LastName:      input.LastName,
@@ -41,28 +65,17 @@ func (r *mutationResolver) CreateUser(ctx context.Context, input *models.CreateU
 		Phone:         input.Phone,
 		IsActive:      input.IsActive,
 	}
-	// need to save to database
 	return user, nil
 }
 func (r *mutationResolver) UpdateUser(ctx context.Context, id string, input *models.UpdateUserInput) (*models.User, error) {
-	// Steps:
-	// 1) Get user by ID, check if it exists
-	// 2) If it exists, then update with given input
-	// 3) Save to database
 	panic("not implemented")
 }
 func (r *mutationResolver) DeleteUser(ctx context.Context, id string) (*models.DeleteItem, error) {
-	// need to remove user by given id
 	panic("not implemented")
 	// return true
 }
 func (r *mutationResolver) CreateRole(ctx context.Context, input *models.CreateRoleInput) (*models.Role, error) {
-	role := &models.Role{
-		Role:        input.Role,
-		Description: input.Description,
-	}
-	// need to save to database
-	return role, nil
+	panic("not implemented")
 }
 func (r *mutationResolver) UpdateRole(ctx context.Context, id string, input *models.UpdateRoleInput) (*models.Role, error) {
 	panic("not implemented")
@@ -71,14 +84,7 @@ func (r *mutationResolver) DeleteRole(ctx context.Context, id string) (*models.D
 	panic("not implemented")
 }
 func (r *mutationResolver) CreatePermission(ctx context.Context, input *models.CreatePermissionInput) (*models.Permission, error) {
-	resource := input.Resource
-	permission := &models.Permission{
-		Permission:  input.Permission,
-		Description: input.Description,
-		Resource:    &resource,
-	}
-	// need to save to database
-	return permission, nil
+	panic("not implemented")
 }
 func (r *mutationResolver) UpdatePermission(ctx context.Context, id string, input *models.UpdatePermissionInput) (*models.Permission, error) {
 	panic("not implemented")
@@ -90,9 +96,6 @@ func (r *mutationResolver) DeletePermission(ctx context.Context, id string) (*mo
 type queryResolver struct{ *Resolver }
 
 func (r *queryResolver) User(ctx context.Context, id string) (*models.User, error) {
-	// Need to improve error handling
-	// Also need to handle timestamps and roles
-	// And verify that this indeed works
 	i, err := strconv.ParseInt(id, 10, 64)
 	if err != nil {
 		return nil, err
@@ -123,7 +126,30 @@ func (r *queryResolver) User(ctx context.Context, id string) (*models.User, erro
 	}, nil
 }
 func (r *queryResolver) UserByEmail(ctx context.Context, email string) (*models.User, error) {
-	panic("not implemented")
+	g, err := r.UserClient.GetUserByEmail(context.Background(), &jsonapi.GetEmailRequest{Email: email})
+	if err != nil {
+		r.Logger.Errorf("Error in getting user by email: %s", err)
+		return nil, err
+	}
+	attr := g.Data.Attributes
+	return &models.User{
+		ID:            strconv.Itoa(int(g.Data.Id)),
+		FirstName:     attr.FirstName,
+		LastName:      attr.LastName,
+		Email:         attr.Email,
+		Organization:  &attr.Organization,
+		FirstAddress:  &attr.FirstAddress,
+		SecondAddress: &attr.SecondAddress,
+		City:          &attr.City,
+		State:         &attr.State,
+		Zipcode:       &attr.Zipcode,
+		Country:       &attr.Country,
+		Phone:         &attr.Phone,
+		IsActive:      attr.IsActive,
+		// CreatedAt:     attr.CreatedAt,
+		// UpdatedAt:     attr.UpdatedAt,
+		// Roles:         &attr.Roles,
+	}, nil
 }
 func (r *queryResolver) ListUsers(ctx context.Context, cursor *string, limit *int, filter *string) (*models.UserListWithCursor, error) {
 	panic("not implemented")
