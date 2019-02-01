9124d7fa7a79f549505042d1fa29ebc00dd7675e
wildlifehexagon <crossthebreeze@gmail.com>
2019-02-01 09:21:13 -0600

    fix: refactor UpdateUser resolver to work with missing fields

--- internal/graphql/resolver/user.go
+++ internal/graphql/resolver/user.go
@@ -97,50 +97,79 @@ func (r *mutationResolver) UpdateUser(ctx context.Context, id string, input *mod
 	if err != nil {
 		return nil, err
 	}
+	attr := &user.UserAttributes{}
+	if input.FirstName != nil {
+		attr.FirstName = *input.FirstName
+	}
+	if input.LastName != nil {
+		attr.LastName = *input.LastName
+	}
+	if input.Organization != nil {
+		attr.Organization = *input.Organization
+	}
+	if input.GroupName != nil {
+		attr.GroupName = *input.GroupName
+	}
+	if input.FirstAddress != nil {
+		attr.FirstAddress = *input.FirstAddress
+	}
+	if input.SecondAddress != nil {
+		attr.SecondAddress = *input.SecondAddress
+	}
+	if input.City != nil {
+		attr.City = *input.City
+	}
+	if input.State != nil {
+		attr.State = *input.State
+	}
+	if input.Zipcode != nil {
+		attr.Zipcode = *input.Zipcode
+	}
+	if input.Country != nil {
+		attr.Country = *input.Country
+	}
+	if input.Phone != nil {
+		attr.Phone = *input.Phone
+	}
+	if input.IsActive != nil {
+		attr.IsActive = *input.IsActive
+	}
 	n, err := r.UserClient.UpdateUser(context.Background(), &user.UpdateUserRequest{
 		Id: i,
 		Data: &user.UpdateUserRequest_Data{
-			Id:   i,
-			Type: "user",
-			Attributes: &user.UserAttributes{
-				FirstName:     *input.FirstName,
-				LastName:      *input.LastName,
-				Organization:  *input.Organization,
-				GroupName:     *input.GroupName,
-				FirstAddress:  *input.FirstAddress,
-				SecondAddress: *input.SecondAddress,
-				City:          *input.City,
-				State:         *input.State,
-				Zipcode:       *input.Zipcode,
-				Country:       *input.Country,
-				Phone:         *input.Phone,
-				IsActive:      *input.IsActive,
-			},
+			Id:         i,
+			Type:       "user",
+			Attributes: attr,
 		},
 	})
 	if err != nil {
-		r.Logger.Errorf("Error updating user from mutation resolver: %s", err)
+		r.Logger.Errorf("Error updating user %d from mutation resolver: %s", n.Data.Id, err)
 		return nil, err
 	}
-	uid := strconv.FormatInt(n.Data.Id, 10)
+	o, err := r.UserClient.GetUser(context.Background(), &jsonapi.GetRequest{Id: i})
+	if err != nil {
+		r.Logger.Errorf("Error fetching recently updated user from mutation resolver: %s", err)
+		return nil, err
+	}
+	uid := strconv.FormatInt(o.Data.Id, 10)
 	user := &models.User{
 		ID:            uid,
-		FirstName:     n.Data.Attributes.FirstName,
-		LastName:      n.Data.Attributes.LastName,
-		Email:         n.Data.Attributes.Email,
-		Organization:  &n.Data.Attributes.Organization,
-		GroupName:     &n.Data.Attributes.GroupName,
-		FirstAddress:  &n.Data.Attributes.FirstAddress,
-		SecondAddress: &n.Data.Attributes.SecondAddress,
-		City:          &n.Data.Attributes.City,
-		State:         &n.Data.Attributes.State,
-		Zipcode:       &n.Data.Attributes.Zipcode,
-		Country:       &n.Data.Attributes.Country,
-		Phone:         &n.Data.Attributes.Phone,
-		IsActive:      n.Data.Attributes.IsActive,
-		CreatedAt:     *n.Data.Attributes.CreatedAt,
-		UpdatedAt:     *n.Data.Attributes.UpdatedAt,
-		// Roles:         &n.Data.Attributes.Roles,
+		FirstName:     o.Data.Attributes.FirstName,
+		LastName:      o.Data.Attributes.LastName,
+		Email:         o.Data.Attributes.Email,
+		Organization:  &o.Data.Attributes.Organization,
+		GroupName:     &o.Data.Attributes.GroupName,
+		FirstAddress:  &o.Data.Attributes.FirstAddress,
+		SecondAddress: &o.Data.Attributes.SecondAddress,
+		City:          &o.Data.Attributes.City,
+		State:         &o.Data.Attributes.State,
+		Zipcode:       &o.Data.Attributes.Zipcode,
+		Country:       &o.Data.Attributes.Country,
+		Phone:         &o.Data.Attributes.Phone,
+		IsActive:      o.Data.Attributes.IsActive,
+		CreatedAt:     *o.Data.Attributes.CreatedAt,
+		UpdatedAt:     *o.Data.Attributes.UpdatedAt,
+		// Roles:         &o.Data.Attributes.Roles,
 	}
 	return user, nil
 }
