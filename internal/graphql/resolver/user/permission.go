6316fe34f937c6167882021759956e75b6ec2237
wildlifehexagon <crossthebreeze@gmail.com>
2019-03-04 17:28:39 -0600

    feat: fill in the individual query resolvers for roles and perms

--- internal/graphql/resolver/user/permission.go
+++ internal/graphql/resolver/user/permission.go
@@ -2,8 +2,11 @@ package resolver
 
 import (
 	"context"
+	"strconv"
 	"time"
 
+	"github.com/dictyBase/apihelpers/aphgrpc"
+	"github.com/dictyBase/go-genproto/dictybaseapis/api/jsonapi"
 	"github.com/dictyBase/go-genproto/dictybaseapis/user"
 	"github.com/dictyBase/graphql-server/internal/graphql/generated"
 
@@ -27,27 +30,38 @@ func (r *mutationResolver) DeletePermission(ctx context.Context, id string) (*mo
 }
 
 func (r *queryResolver) Permission(ctx context.Context, id string) (*user.Permission, error) {
-	panic("not implemented")
+	i, err := strconv.ParseInt(id, 10, 64)
+	if err != nil {
+		r.Logger.Errorf("error in parsing string %s to int %s", id, err)
+		return nil, err
+	}
+	g, err := r.PermissionClient.GetPermission(ctx, &jsonapi.GetRequestWithFields{Id: i})
+	if err != nil {
+		r.Logger.Errorf("error in getting permission by ID %d: %s", i, err)
+		return nil, err
+	}
+	r.Logger.Infof("successfully found permission with ID %s", id)
+	return g, nil
 }
 func (r *queryResolver) ListPermissions(ctx context.Context) ([]user.Permission, error) {
 	panic("not implemented")
 }
 
 func (r *permissionResolver) ID(ctx context.Context, obj *user.Permission) (string, error) {
-	panic("not implemented")
+	return strconv.FormatInt(obj.Data.Id, 10), nil
 }
 func (r *permissionResolver) Permission(ctx context.Context, obj *user.Permission) (string, error) {
-	panic("not implemented")
+	return obj.Data.Attributes.Permission, nil
 }
 func (r *permissionResolver) Description(ctx context.Context, obj *user.Permission) (string, error) {
-	panic("not implemented")
+	return obj.Data.Attributes.Description, nil
 }
 func (r *permissionResolver) CreatedAt(ctx context.Context, obj *user.Permission) (time.Time, error) {
-	panic("not implemented")
+	return aphgrpc.ProtoTimeStamp(obj.Data.Attributes.CreatedAt), nil
 }
 func (r *permissionResolver) UpdatedAt(ctx context.Context, obj *user.Permission) (time.Time, error) {
-	panic("not implemented")
+	return aphgrpc.ProtoTimeStamp(obj.Data.Attributes.CreatedAt), nil
 }
 func (r *permissionResolver) Resource(ctx context.Context, obj *user.Permission) (*string, error) {
-	panic("not implemented")
+	return &obj.Data.Attributes.Resource, nil
 }
--- internal/graphql/resolver/user/role.go
+++ internal/graphql/resolver/user/role.go
@@ -2,8 +2,11 @@ package resolver
 
 import (
 	"context"
+	"strconv"
 	"time"
 
+	"github.com/dictyBase/apihelpers/aphgrpc"
+	"github.com/dictyBase/go-genproto/dictybaseapis/api/jsonapi"
 	"github.com/dictyBase/go-genproto/dictybaseapis/user"
 	"github.com/dictyBase/graphql-server/internal/graphql/generated"
 
@@ -26,26 +29,37 @@ func (r *mutationResolver) DeleteRole(ctx context.Context, id string) (*models.D
 	panic("not implemented")
 }
 func (r *queryResolver) Role(ctx context.Context, id string) (*user.Role, error) {
-	panic("not implemented")
+	i, err := strconv.ParseInt(id, 10, 64)
+	if err != nil {
+		r.Logger.Errorf("error in parsing string %s to int %s", id, err)
+		return nil, err
+	}
+	g, err := r.RoleClient.GetRole(ctx, &jsonapi.GetRequest{Id: i})
+	if err != nil {
+		r.Logger.Errorf("error in getting role by ID %d: %s", i, err)
+		return nil, err
+	}
+	r.Logger.Infof("successfully found role with ID %s", id)
+	return g, nil
 }
 func (r *queryResolver) ListRoles(ctx context.Context) ([]user.Role, error) {
 	panic("not implemented")
 }
 
 func (r *roleResolver) ID(ctx context.Context, obj *user.Role) (string, error) {
-	panic("not implemented")
+	return strconv.FormatInt(obj.Data.Id, 10), nil
 }
 func (r *roleResolver) Role(ctx context.Context, obj *user.Role) (string, error) {
-	panic("not implemented")
+	return obj.Data.Attributes.Role, nil
 }
 func (r *roleResolver) Description(ctx context.Context, obj *user.Role) (string, error) {
-	panic("not implemented")
+	return obj.Data.Attributes.Description, nil
 }
 func (r *roleResolver) CreatedAt(ctx context.Context, obj *user.Role) (time.Time, error) {
-	panic("not implemented")
+	return aphgrpc.ProtoTimeStamp(obj.Data.Attributes.CreatedAt), nil
 }
 func (r *roleResolver) UpdatedAt(ctx context.Context, obj *user.Role) (time.Time, error) {
-	panic("not implemented")
+	return aphgrpc.ProtoTimeStamp(obj.Data.Attributes.UpdatedAt), nil
 }
 func (r *roleResolver) Permissions(ctx context.Context, obj *user.Role) ([]user.Permission, error) {
 	panic("not implemented")
