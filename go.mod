5e9850ed6d5d45d5508d2729b2abd352504af787
wildlifehexagon <crossthebreeze@gmail.com>
2019-03-13 15:19:04 -0500

    feat: write generator script to download schema from github repo then remove them at the end

--- go.mod
+++ go.mod
@@ -12,6 +12,8 @@ require (
 	github.com/garyburd/redigo v1.6.0 // indirect
 	github.com/gogo/protobuf v1.2.0 // indirect
 	github.com/golang/protobuf v1.2.0
+	github.com/google/go-github v17.0.0+incompatible
+	github.com/google/go-querystring v1.0.0 // indirect
 	github.com/gorilla/websocket v1.4.0 // indirect
 	github.com/grpc-ecosystem/grpc-gateway v1.6.4 // indirect
 	github.com/hashicorp/golang-lru v0.5.0 // indirect
--- scripts/gqlgen.go
+++ scripts/gqlgen.go
@@ -2,8 +2,66 @@
 
 package main
 
-import "github.com/99designs/gqlgen/cmd"
+import (
+	"context"
+	"encoding/base64"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/99designs/gqlgen/cmd"
+	"github.com/google/go-github/github"
+)
 
 func main() {
+	ctx := context.Background()
+	dir := "../../../api"
+	grdir := "../genresolver"
+	grfile := filepath.Join(grdir, "/genresolver.go")
+	// if generated resolver file already exists, remove it
+	if _, err := os.Stat(grfile); err == nil {
+		os.Remove(grfile)
+	}
+	// create temporary directory for genresolver if it doesn't exist
+	if _, err := os.Stat(grdir); os.IsNotExist(err) {
+		os.Mkdir(grdir, 0777)
+	}
+	// create temporary directory for schema if it doesn't exist
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		os.Mkdir(dir, 0777)
+	}
+	// remove temp directory and its contents at end of script
+	defer os.RemoveAll(dir)
+	// initialize github client
+	client := github.NewClient(nil)
+	// get all files from our schema repo
+	_, files, _, err := client.Repositories.GetContents(ctx, "dictyBase", "graphql-schema", "/", nil)
+	if err != nil {
+		log.Fatalf("error in getting graphql schema", err)
+	}
+	// loop through these files
+	for _, n := range files {
+		// get each individual file from our repo
+		file, _, _, err := client.Repositories.GetContents(ctx, "dictyBase", "graphql-schema", n.GetName(), nil)
+		if err != nil {
+			log.Fatalf("error in getting individual schema file", err)
+		}
+		// decode file contents
+		d, err := base64.StdEncoding.DecodeString(*file.Content)
+		if err != nil {
+			log.Fatalf("error decoding github file contents", err)
+		}
+		// create desired file path
+		fp := filepath.Join(dir, "/", file.GetName())
+		// write the github file content into its own local file
+		err = ioutil.WriteFile(fp, d, 0777)
+		if err != nil {
+			log.Fatalf("error writing file", err)
+		}
+		fmt.Printf("successfully wrote file %s\n", fp)
+	}
 	cmd.Execute()
+	fmt.Printf("successfully removed temporary directory %s", dir)
 }
