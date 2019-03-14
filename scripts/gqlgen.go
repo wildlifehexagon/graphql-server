8fa06d701bec899040dc6b6aee69001736400ce5
wildlifehexagon <crossthebreeze@gmail.com>
2019-03-14 12:24:41 -0500

    refactor: update generator script to use absolute path, add checks before removing folder

--- scripts/gqlgen.go
+++ scripts/gqlgen.go
@@ -10,6 +10,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/99designs/gqlgen/cmd"
 	"github.com/google/go-github/github"
@@ -18,51 +19,43 @@ import (
 func main() {
 	ctx := context.Background()
 	dir := "../../../api"
+	apath, err := filepath.Abs(dir)
+	if err != nil {
+		log.Fatalf("could not get absolute path %s", err)
+	}
 	grdir := "../genresolver"
 	grfile := filepath.Join(grdir, "/genresolver.go")
 	// if generated resolver file already exists, remove it
 	if _, err := os.Stat(grfile); err == nil {
 		os.Remove(grfile)
+		fmt.Printf("successfully removed genresolver file %s\n", grfile)
 	}
-	// create temporary directory for genresolver if it doesn't exist
+	// create temporary directory for genresolver
 	if _, err := os.Stat(grdir); os.IsNotExist(err) {
 		// 0777 denotes read, write, & execute for owner, group and others
 		os.Mkdir(grdir, 0777)
 	}
 	// create temporary directory for schema if it doesn't exist
-	if _, err := os.Stat(dir); os.IsNotExist(err) {
-		os.Mkdir(dir, 0777)
+	if _, err := os.Stat(apath); os.IsNotExist(err) {
+		os.Mkdir(apath, 0777)
 	}
-	// remove temp directory and its contents at end of script
-	defer func() {
-		err := os.RemoveAll(dir)
-		if err != nil {
-			log.Fatalf("error removing temporary schema directory %s", err)
-		}
-		fmt.Printf("successfully removed temporary schema directory %s\n", dir)
-	}()
-	// initialize github client
+	defer cleanup(apath)
 	client := github.NewClient(nil)
-	// get all files from our schema repo
 	_, files, _, err := client.Repositories.GetContents(ctx, "dictyBase", "graphql-schema", "/", nil)
 	if err != nil {
 		log.Fatalf("error in getting graphql schema", err)
 	}
-	// loop through these files
 	for _, n := range files {
-		// get each individual file from our repo
 		file, _, _, err := client.Repositories.GetContents(ctx, "dictyBase", "graphql-schema", n.GetName(), nil)
 		if err != nil {
 			log.Fatalf("error in getting individual schema file", err)
 		}
-		// decode file contents
+		// need to decode file contents
 		d, err := base64.StdEncoding.DecodeString(*file.Content)
 		if err != nil {
 			log.Fatalf("error decoding github file contents", err)
 		}
-		// create desired file path
 		fp := filepath.Join(dir, "/", file.GetName())
-		// write the github file content into its own local file
 		err = ioutil.WriteFile(fp, d, 0777)
 		if err != nil {
 			log.Fatalf("error writing file", err)
@@ -70,4 +63,25 @@ func main() {
 		fmt.Printf("successfully wrote file %s\n", fp)
 	}
 	cmd.Execute()
+}
+
+func cleanup(path string) {
+	s := strings.Split(path, "/")
+	tmpfile := path + "/query.graphql"
+	if _, err := os.Stat(tmpfile); err == nil {
+		if s[len(s)-2] == "graphql-server" {
+			err = os.RemoveAll(path)
+			if err != nil {
+				fmt.Printf("unable to remove temporary schema directory %s", err)
+				os.Exit(1)
+			}
+			fmt.Printf("successfully removed temporary schema directory %s\n", path)
+		} else {
+			fmt.Printf("folder path does not match")
+			os.Exit(1)
+		}
+	} else {
+		fmt.Printf("unable to remove temporary schema directory")
+		os.Exit(1)
+	}
 }
