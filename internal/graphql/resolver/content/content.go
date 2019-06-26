621be2ad1501185b56ea9bcde84c6514bf6142e2
wildlifehexagon <crossthebreeze@gmail.com>
2019-06-26 12:02:18 -0500

    feat: fill out most unshared content resolvers

--- internal/graphql/resolver/content/content.go
+++ internal/graphql/resolver/content/content.go
@@ -2,8 +2,10 @@ package content
 
 import (
 	"context"
+	"strconv"
 	"time"
 
+	"github.com/dictyBase/apihelpers/aphgrpc"
 	pb "github.com/dictyBase/go-genproto/dictybaseapis/content"
 	"github.com/sirupsen/logrus"
 )
@@ -14,13 +16,13 @@ type ContentResolver struct {
 }
 
 func (r *ContentResolver) ID(ctx context.Context, obj *pb.Content) (string, error) {
-	panic("not implemented")
+	return strconv.FormatInt(obj.Data.Id, 10), nil
 }
 func (r *ContentResolver) Name(ctx context.Context, obj *pb.Content) (string, error) {
-	panic("not implemented")
+	return obj.Data.Attributes.Name, nil
 }
 func (r *ContentResolver) Slug(ctx context.Context, obj *pb.Content) (string, error) {
-	panic("not implemented")
+	return obj.Data.Attributes.Slug, nil
 }
 func (r *ContentResolver) CreatedBy(ctx context.Context, obj *pb.Content) (string, error) {
 	panic("not implemented")
@@ -29,14 +31,16 @@ func (r *ContentResolver) UpdatedBy(ctx context.Context, obj *pb.Content) (strin
 	panic("not implemented")
 }
 func (r *ContentResolver) CreatedAt(ctx context.Context, obj *pb.Content) (*time.Time, error) {
-	panic("not implemented")
+	time := aphgrpc.ProtoTimeStamp(obj.Data.Attributes.CreatedAt)
+	return &time, nil
 }
 func (r *ContentResolver) UpdatedAt(ctx context.Context, obj *pb.Content) (*time.Time, error) {
-	panic("not implemented")
+	time := aphgrpc.ProtoTimeStamp(obj.Data.Attributes.UpdatedAt)
+	return &time, nil
 }
 func (r *ContentResolver) Content(ctx context.Context, obj *pb.Content) (string, error) {
-	panic("not implemented")
+	return obj.Data.Attributes.Content, nil
 }
 func (r *ContentResolver) Namespace(ctx context.Context, obj *pb.Content) (string, error) {
-	panic("not implemented")
+	return obj.Data.Attributes.Namespace, nil
 }
