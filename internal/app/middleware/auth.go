6b349c1addea590d5abd828154a044aacb3982c1
wildlifehexagon <crossthebreeze@gmail.com>
2020-02-05 10:15:11 -0600

    refactor: rewrite AuthMiddleware to return http.Handler

--- internal/app/middleware/auth.go
+++ internal/app/middleware/auth.go
@@ -22,21 +22,19 @@ func (w *authResponseWriter) Write(b []byte) (int, error) {
 	return w.ResponseWriter.Write(b)
 }
 
-func AuthMiddleWare(next http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+func AuthMiddleWare(h http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
 		arw := authResponseWriter{w, ""}
 		c, err := r.Cookie("refresh-token")
 		// let unauthenticated users in
 		if err != nil || c == nil {
-			next.ServeHTTP(w, r)
-			return
+			h.ServeHTTP(w, r)
+		} else {
+			arw.cookieStr = c.Value
+			newCtx := context.WithValue(ctx, AuthContextKey, &w)
+			h.ServeHTTP(w, r.WithContext(newCtx))
 		}
-		arw.cookieStr = c.Value
-		// store cookie in context
-		ctx := context.WithValue(r.Context(), AuthContextKey, &w)
-		// call with new context
-		r = r.WithContext(ctx)
-		// execute next
-		next(&arw, r)
 	}
+	return http.HandlerFunc(fn)
 }
