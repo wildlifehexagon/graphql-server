bbacc2c9ddaf622ee21ac58c6bb22d74283f66a6
wildlifehexagon <crossthebreeze@gmail.com>
2020-02-05 11:11:16 -0600

    refactor: add comparison between refresh token values

--- internal/app/middleware/auth.go
+++ internal/app/middleware/auth.go
@@ -6,35 +6,42 @@ import (
 	"net/http"
 )
 
-var AuthContextKey = "refreshToken"
+const (
+	AuthContextKey = "refreshToken"
+	CookieStr      = "refresh-token"
+)
 
 type authResponseWriter struct {
 	http.ResponseWriter
-	cookieStr string
+	refreshTokenFromCookie string
+	refreshTokenToResolver string
 }
 
 func (w *authResponseWriter) Write(b []byte) (int, error) {
-	http.SetCookie(w, &http.Cookie{
-		Name:     "refresh-token",
-		Value:    w.cookieStr,
-		HttpOnly: true,
-	})
+	if w.refreshTokenToResolver != w.refreshTokenFromCookie {
+		http.SetCookie(w, &http.Cookie{
+			Name:     CookieStr,
+			Value:    w.refreshTokenFromCookie,
+			HttpOnly: true,
+		})
+	}
 	return w.ResponseWriter.Write(b)
 }
 
-func AuthMiddleWare(h http.Handler) http.Handler {
-	fn := func(w http.ResponseWriter, r *http.Request) {
+func AuthMiddleWare(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
-		arw := authResponseWriter{w, ""}
-		c, err := r.Cookie("refresh-token")
-		// let unauthenticated users in
+		arw := authResponseWriter{w, "", ""}
+		// get refresh token from cookie
+		c, err := r.Cookie(CookieStr)
 		if err != nil || c == nil {
-			h.ServeHTTP(w, r)
-		} else {
-			arw.cookieStr = c.Value
-			newCtx := context.WithValue(ctx, AuthContextKey, &w)
-			h.ServeHTTP(w, r.WithContext(newCtx))
+			next.ServeHTTP(w, r.WithContext(ctx))
+			return
 		}
+		arw.refreshTokenFromCookie = c.Value
+		arw.refreshTokenToResolver = c.Value
+		newCtx := context.WithValue(ctx, AuthContextKey, &arw.refreshTokenToResolver)
+		// executing next
+		next(&arw, r.WithContext(newCtx))
 	}
-	return http.HandlerFunc(fn)
 }
