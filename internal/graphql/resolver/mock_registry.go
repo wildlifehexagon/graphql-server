2d5acff8b38d72789b63b1e34b4c626d57317458
Siddhartha Basu <biosidd@gmail.com>
2020-04-23 10:45:26 -0500

    refactor: return mocked order client through mocked registry interface

--- internal/graphql/resolver/mock_registry.go
+++ internal/graphql/resolver/mock_registry.go
@@ -8,56 +8,95 @@ import (
 	"github.com/dictyBase/go-genproto/dictybaseapis/order"
 	"github.com/dictyBase/go-genproto/dictybaseapis/stock"
 	"github.com/dictyBase/go-genproto/dictybaseapis/user"
+	"github.com/dictyBase/graphql-server/internal/graphql/mocks"
+	"github.com/golang/protobuf/ptypes"
+	"github.com/stretchr/testify/mock"
 	"google.golang.org/grpc"
 )
 
-func AddAPIEndpoint(key, endpoint string) {
+func mockOrder() *order.Order {
+	return &order.Order{
+		Data: &order.Order_Data{
+			Type: "order",
+			Id:   "999",
+			Attributes: &order.OrderAttributes{
+				CreatedAt:        ptypes.TimestampNow(),
+				UpdatedAt:        ptypes.TimestampNow(),
+				Courier:          "USPS",
+				CourierAccount:   "123456",
+				Comments:         "first order",
+				Payment:          "credit",
+				PurchaseOrderNum: "987654",
+				Status:           0, // in preparation
+				Consumer:         "art@vandelayindustries.com",
+				Payer:            "george@costanza.com",
+				Purchaser:        "thatsgold@jerry.org",
+				Items:            []string{"DBS123456"},
+			},
+		},
+	}
+}
+
+func mockedOrderClient() *mocks.OrderServiceClient {
+	mockedOrderClient := new(mocks.OrderServiceClient)
+	mockedOrderClient.On(
+		"GetOrder",
+		mock.Anything,
+		mock.AnythingOfType("*order.OrderId"),
+	).Return(mockOrder(), nil)
+	return mockedOrderClient
+}
+
+type mockRegistry struct{}
+
+func (mr *mockRegistry) AddAPIEndpoint(key, endpoint string) {
 }
 
 // AddAPIClient adds a new entry to the hashmap
-func AddAPIConnection(key string, conn *grpc.ClientConn) {
+func (mr *mockRegistry) AddAPIConnection(key string, conn *grpc.ClientConn) {
 }
 
 // GetAPIClient looks up a client in the hashmap
-func GetAPIConnection(key string) (conn *grpc.ClientConn) {
+func (mr *mockRegistry) GetAPIConnection(key string) (conn *grpc.ClientConn) {
+	return &grpc.ClientConn{}
 }
 
-func GetUserClient(key string) user.UserServiceClient {
-	return user.UserServiceClient
+func (mr *mockRegistry) GetUserClient(key string) user.UserServiceClient {
+	return user.NewUserServiceClient(mr.GetAPIConnection(key))
 }
 
-func GetRoleClient(key string) user.RoleServiceClient {
-	return user.RoleServiceClient
+func (mr *mockRegistry) GetRoleClient(key string) user.RoleServiceClient {
+	return user.NewRoleServiceClient(mr.GetAPIConnection(key))
 }
 
-func GetPermissionClient(key string) user.PermissionServiceClient {
-	return user.PermissionServiceClient
+func (mr *mockRegistry) GetPermissionClient(key string) user.PermissionServiceClient {
+	return user.NewPermissionServiceClient(mr.GetAPIConnection(key))
 }
 
-func GetStockClient(key string) stock.StockServiceClient {
-	return stock.StockServiceClient
+func (mr *mockRegistry) GetStockClient(key string) stock.StockServiceClient {
+	return stock.NewStockServiceClient(mr.GetAPIConnection(key))
 }
 
-func GetOrderClient(key string) order.OrderServiceClient {
-	return order.OrderServiceClient
+func (mr *mockRegistry) GetOrderClient(key string) order.OrderServiceClient {
+	return mockedOrderClient()
 }
 
-func GetContentClient(key string) content.ContentServiceClient {
-	return content.ContentServiceClient
+func (mr *mockRegistry) GetContentClient(key string) content.ContentServiceClient {
+	return content.NewContentServiceClient(mr.GetAPIConnection(key))
 }
 
-func GetAnnotationClient(key string) annotation.TaggedAnnotationServiceClient {
-	return annotation.TaggedAnnotationServiceClient
+func (mr *mockRegistry) GetAnnotationClient(key string) annotation.TaggedAnnotationServiceClient {
+	return annotation.NewTaggedAnnotationServiceClient(mr.GetAPIConnection(key))
 }
 
-func GetAuthClient(key string) auth.AuthServiceClient {
-	return auth.AuthServiceClient
+func (mr *mockRegistry) GetAuthClient(key string) auth.AuthServiceClient {
+	return auth.NewAuthServiceClient(mr.GetAPIConnection(key))
 }
 
-func GetIdentityClient(key string) identity.IdentityServiceClient {
-	return identity.IdentityServiceClient
+func (mr *mockRegistry) GetIdentityClient(key string) identity.IdentityServiceClient {
+	return identity.NewIdentityServiceClient(mr.GetAPIConnection(key))
 }
 
-func GetAPIEndpoint(key string) string {
+func (mr mockRegistry) GetAPIEndpoint(key string) string {
 	return ""
 }
