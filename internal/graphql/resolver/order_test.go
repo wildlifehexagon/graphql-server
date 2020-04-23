537d8478bccaccd1a5ae2aa380ccc39d8f7641c8
wildlifehexagon <crossthebreeze@gmail.com>
2020-04-23 12:48:42 -0500

    test: add all assertions for individual order query

--- internal/graphql/resolver/order_test.go
+++ internal/graphql/resolver/order_test.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"testing"
 
+	"github.com/dictyBase/go-genproto/dictybaseapis/order"
+
 	"github.com/dictyBase/graphql-server/internal/graphql/mocks"
 	"github.com/stretchr/testify/assert"
 )
@@ -14,7 +16,17 @@ func TestOrder(t *testing.T) {
 		Registry: &mocks.MockRegistry{},
 		Logger:   mocks.TestLogger(),
 	}
-	_, err := ord.Order(context.Background(), "999")
+	o, err := ord.Order(context.Background(), "999")
 	assert.NoError(err, "expect no error from getting order information")
-	// assert.Exactly(name, "DBS0236922", "should match systematic name")
+	assert.Exactly(o.Data.Id, "999", "should match id")
+	assert.Exactly(o.Data.Attributes.Courier, "USPS", "should match courier")
+	assert.Exactly(o.Data.Attributes.CourierAccount, "123456", "should match courier account")
+	assert.Exactly(o.Data.Attributes.Comments, "first order", "should match comments")
+	assert.Exactly(o.Data.Attributes.Payment, "credit", "should match payment")
+	assert.Exactly(o.Data.Attributes.PurchaseOrderNum, "987654", "should match purchase order number")
+	assert.Exactly(o.Data.Attributes.Status, order.OrderStatus_In_preparation, "should match status")
+	assert.Exactly(o.Data.Attributes.Consumer, "art@vandelayindustries.com", "should match consumer")
+	assert.Exactly(o.Data.Attributes.Payer, "george@costanza.com", "should match payer")
+	assert.Exactly(o.Data.Attributes.Purchaser, "thatsgold@jerry.org", "should match purchaser")
+	assert.ElementsMatch(o.Data.Attributes.Items, []string{"DBS123456"}, "should match items")
 }
