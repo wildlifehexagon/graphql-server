4af3f96f6a8e7d86eba80d49f472b01f144ca936
wildlifehexagon <crossthebreeze@gmail.com>
2020-04-29 10:26:13 -0500

    test: add more tests for annotations

--- internal/graphql/mocks/annotation.go
+++ internal/graphql/mocks/annotation.go
@@ -24,17 +24,95 @@ var MockSysNameAnno = &annotation.TaggedAnnotation{
 	},
 }
 
+var MockGenModAnno = &annotation.TaggedAnnotation{
+	Data: &annotation.TaggedAnnotation_Data{
+		Type: "annotation",
+		Id:   "123456",
+		Attributes: &annotation.TaggedAnnotationAttributes{
+			Value:     "exogenous mutation",
+			EntryId:   "DBS0236922",
+			CreatedBy: "dsc@dictycr.org",
+			CreatedAt: ptypes.TimestampNow(),
+			Tag:       registry.MuttypeTag,
+			Ontology:  registry.DictyAnnoOntology,
+			Version:   1,
+		},
+	},
+}
+
+var MockMutMethodAnno = &annotation.TaggedAnnotation{
+	Data: &annotation.TaggedAnnotation_Data{
+		Type: "annotation",
+		Id:   "123456",
+		Attributes: &annotation.TaggedAnnotationAttributes{
+			Value:     "Random Insertion",
+			EntryId:   "DBS0236922",
+			CreatedBy: "dsc@dictycr.org",
+			CreatedAt: ptypes.TimestampNow(),
+			Tag:       registry.MutmethodTag,
+			Ontology:  registry.DictyAnnoOntology,
+			Version:   1,
+		},
+	},
+}
+
+var MockGenotypeAnno = &annotation.TaggedAnnotation{
+	Data: &annotation.TaggedAnnotation_Data{
+		Type: "annotation",
+		Id:   "123456",
+		Attributes: &annotation.TaggedAnnotationAttributes{
+			Value:     "axeA1,axeB1,axeC1,sadA-[sadA-KO],[pSadA-GFP],bsR,neoR",
+			EntryId:   "DBS0236922",
+			CreatedBy: "dsc@dictycr.org",
+			CreatedAt: ptypes.TimestampNow(),
+			Tag:       registry.GenoTag,
+			Ontology:  registry.DictyAnnoOntology,
+			Version:   1,
+		},
+	},
+}
+
 func MockedAnnotationClient() *clients.TaggedAnnotationServiceClient {
 	mockedAnnoClient := new(clients.TaggedAnnotationServiceClient)
 	return mockedAnnoClient
 }
 
-func MockedSysAnnoClient() *clients.TaggedAnnotationServiceClient {
+func MockedSysNameAnnoClient() *clients.TaggedAnnotationServiceClient {
 	mockedAnnoClient := new(clients.TaggedAnnotationServiceClient)
 	mockedAnnoClient.On(
 		"GetEntryAnnotation",
 		mock.Anything,
 		mock.AnythingOfType("*annotation.EntryAnnotationRequest"),
 	).Return(MockSysNameAnno, nil)
+	return mockedAnnoClient
+}
+
+func MockedGenModClient() *clients.TaggedAnnotationServiceClient {
+	mockedAnnoClient := new(clients.TaggedAnnotationServiceClient)
+	mockedAnnoClient.On(
+		"GetEntryAnnotation",
+		mock.Anything,
+		mock.AnythingOfType("*annotation.EntryAnnotationRequest"),
+	).Return(MockGenModAnno, nil)
+	return mockedAnnoClient
+}
+
+func MockedMutMethodClient() *clients.TaggedAnnotationServiceClient {
+	mockedAnnoClient := new(clients.TaggedAnnotationServiceClient)
+	mockedAnnoClient.On(
+		"GetEntryAnnotation",
+		mock.Anything,
+		mock.AnythingOfType("*annotation.EntryAnnotationRequest"),
+	).Return(MockMutMethodAnno, nil)
+	return mockedAnnoClient
+}
+
+func MockedGenotypeClient() *clients.TaggedAnnotationServiceClient {
+	mockedAnnoClient := new(clients.TaggedAnnotationServiceClient)
+	mockedAnnoClient.On(
+		"GetEntryAnnotation",
+		mock.Anything,
+		mock.AnythingOfType("*annotation.EntryAnnotationRequest"),
+	).Return(MockGenotypeAnno, nil)
 	return mockedAnnoClient
 }
--- internal/graphql/resolver/stock/strain_test.go
+++ internal/graphql/resolver/stock/strain_test.go
@@ -8,8 +8,19 @@ import (
 	"github.com/dictyBase/graphql-server/internal/graphql/models"
 
 	"github.com/dictyBase/graphql-server/internal/graphql/mocks"
+	"github.com/dictyBase/graphql-server/internal/graphql/mocks/clients"
 	"github.com/stretchr/testify/assert"
 )
+
+func strainResolver(annoClient *clients.TaggedAnnotationServiceClient) *StrainResolver {
+	return &StrainResolver{
+		Client:           mocks.MockedStockClient(),
+		UserClient:       mocks.MockedUserClient(),
+		AnnotationClient: annoClient,
+		Registry:         &mocks.MockRegistry{},
+		Logger:           mocks.TestLogger(),
+	}
+}
 
 var mockInput = &models.Strain{
 	Data: &stock.Strain_Data{
@@ -20,15 +31,39 @@ var mockInput = &models.Strain{
 }
 
 func TestSystematicName(t *testing.T) {
+	t.Parallel()
 	assert := assert.New(t)
-	r := &StrainResolver{
-		Client:           mocks.MockedStockClient(),
-		UserClient:       mocks.MockedUserClient(),
-		AnnotationClient: mocks.MockedSysAnnoClient(),
-		Registry:         &mocks.MockRegistry{},
-		Logger:           mocks.TestLogger(),
-	}
+	r := strainResolver(mocks.MockedSysNameAnnoClient())
 	sn, err := r.SystematicName(context.Background(), mockInput)
 	assert.NoError(err, "expect no error from getting systematic name")
 	assert.Exactly(sn, mocks.MockSysNameAnno.Data.Attributes.Value, "should match systematic name")
+}
+
+func TestGeneticModification(t *testing.T) {
+	t.Parallel()
+	assert := assert.New(t)
+	r := strainResolver(mocks.MockedGenModClient())
+	g, err := r.GeneticModification(context.Background(), mockInput)
+	assert.NoError(err, "expect no error from getting genetic modification")
+	assert.Exactly(g, &mocks.MockGenModAnno.Data.Attributes.Value, "should match genetic modification")
+}
+
+func TestMutagenesisMethod(t *testing.T) {
+	t.Parallel()
+	assert := assert.New(t)
+	r := strainResolver(mocks.MockedMutMethodClient())
+	m, err := r.MutagenesisMethod(context.Background(), mockInput)
+	assert.NoError(err, "expect no error from getting mutagenesis method")
+	assert.Exactly(m, &mocks.MockMutMethodAnno.Data.Attributes.Value, "should match mutagenesis method")
+}
+
+func TestGenotypes(t *testing.T) {
+	t.Parallel()
+	assert := assert.New(t)
+	r := strainResolver(mocks.MockedMutMethodClient())
+	g, err := r.Genotypes(context.Background(), mockInput)
+	gl := []*string{}
+	gl = append(gl, &mocks.MockMutMethodAnno.Data.Attributes.Value)
+	assert.NoError(err, "expect no error from getting genotypes")
+	assert.Exactly(g, gl, "should match genotypes")
 }
