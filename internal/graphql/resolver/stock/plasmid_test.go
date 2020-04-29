f55f2e8cf5051badd3da92a057939609e71574bd
wildlifehexagon <crossthebreeze@gmail.com>
2020-04-29 16:21:36 -0500

    test: add more tests for individual plasmid resolvers

--- internal/graphql/resolver/stock/plasmid_test.go
+++ internal/graphql/resolver/stock/plasmid_test.go
@@ -30,11 +30,65 @@ var mockPlasmidInput = &models.Plasmid{
 	},
 }
 
+func TestID(t *testing.T) {
+	t.Parallel()
+	assert := assert.New(t)
+	r := plasmidResolver(mocks.MockedAnnotationClient())
+	p, err := r.ID(context.Background(), mockPlasmidInput)
+	assert.NoError(err, "expect no error from getting plasmid id")
+	assert.Exactly(p, mockPlasmidInput.Data.Id, "should match id")
+}
+
+func TestSummary(t *testing.T) {
+	t.Parallel()
+	assert := assert.New(t)
+	r := plasmidResolver(mocks.MockedAnnotationClient())
+	p, err := r.Summary(context.Background(), mockPlasmidInput)
+	assert.NoError(err, "expect no error from getting summary")
+	assert.Exactly(p, &mockPlasmidInput.Data.Attributes.Summary, "should match summary")
+}
+
+func TestEditableSummary(t *testing.T) {
+	t.Parallel()
+	assert := assert.New(t)
+	r := plasmidResolver(mocks.MockedAnnotationClient())
+	p, err := r.EditableSummary(context.Background(), mockPlasmidInput)
+	assert.NoError(err, "expect no error from getting editable summary")
+	assert.Exactly(p, &mockPlasmidInput.Data.Attributes.EditableSummary, "should match editable summary")
+}
+
+func TestDepositor(t *testing.T) {
+	t.Parallel()
+	assert := assert.New(t)
+	r := plasmidResolver(mocks.MockedAnnotationClient())
+	p, err := r.Depositor(context.Background(), mockPlasmidInput)
+	assert.NoError(err, "expect no error from getting depositor")
+	assert.Exactly(p, mockPlasmidInput.Data.Attributes.Depositor, "should match depositor")
+}
+
+func TestSequence(t *testing.T) {
+	t.Parallel()
+	assert := assert.New(t)
+	r := plasmidResolver(mocks.MockedAnnotationClient())
+	p, err := r.Sequence(context.Background(), mockPlasmidInput)
+	assert.NoError(err, "expect no error from getting plasmid sequence")
+	assert.Exactly(p, &mockPlasmidInput.Data.Attributes.Sequence, "should match sequence")
+}
+
+func TestName(t *testing.T) {
+	t.Parallel()
+	assert := assert.New(t)
+	r := plasmidResolver(mocks.MockedAnnotationClient())
+	p, err := r.Name(context.Background(), mockPlasmidInput)
+	assert.NoError(err, "expect no error from getting plasmid name")
+	assert.Exactly(p, mockPlasmidInput.Data.Attributes.Name, "should match name")
+}
+
 func TestPlasmidInStock(t *testing.T) {
 	t.Parallel()
 	assert := assert.New(t)
 	r := plasmidResolver(mocks.MockedInStockClient())
-	g, err := r.InStock(context.Background(), mockPlasmidInput)
+	p, err := r.InStock(context.Background(), mockPlasmidInput)
 	assert.NoError(err, "expect no error from getting plasmid inventory")
-	assert.True(g, "should return true after finding inventory")
+	assert.True(p, "should return true after finding inventory")
 }
