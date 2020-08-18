1ac05fa82a24a0babb3c3ceebf9756fa17f549d3
wildlifehexagon <crossthebreeze@gmail.com>
2020-08-18 11:32:40 -0500

    test: add hashmap to mock registry

--- internal/graphql/mocks/registry_mock.go
+++ internal/graphql/mocks/registry_mock.go
@@ -9,24 +9,34 @@ import (
 	"github.com/dictyBase/go-genproto/dictybaseapis/stock"
 	"github.com/dictyBase/go-genproto/dictybaseapis/user"
 	"github.com/dictyBase/graphql-server/internal/repository"
-	"github.com/dictyBase/graphql-server/internal/repository/redis"
+	"github.com/emirpasic/gods/maps/hashmap"
 	"google.golang.org/grpc"
 )
 
-type MockRegistry struct{}
+type MockRegistry struct {
+	connMap *hashmap.Map
+}
 
 func (mr *MockRegistry) AddAPIEndpoint(key, endpoint string) {
+	mr.connMap.Put(key, endpoint)
 }
 
 func (mr *MockRegistry) AddAPIConnection(key string, conn *grpc.ClientConn) {
+	mr.connMap.Put(key, conn)
 }
 
 func (mr *MockRegistry) AddRepository(key string, st repository.Repository) {
+	mr.connMap.Put(key, st)
 }
 
 // GetAPIClient looks up a client in the hashmap
 func (mr *MockRegistry) GetAPIConnection(key string) (conn *grpc.ClientConn) {
-	return &grpc.ClientConn{}
+	v, ok := mr.connMap.Get(key)
+	if !ok {
+		panic("could not get grpc client connection")
+	}
+	conn, _ = v.(*grpc.ClientConn)
+	return conn
 }
 
 func (mr *MockRegistry) GetUserClient(key string) user.UserServiceClient {
@@ -66,9 +76,13 @@ func (mr *MockRegistry) GetIdentityClient(key string) identity.IdentityServiceCl
 }
 
 func (mr MockRegistry) GetAPIEndpoint(key string) string {
-	return key
+	v, _ := mr.connMap.Get(key)
+	endpoint, _ := v.(string)
+	return endpoint
 }
 
 func (mr MockRegistry) GetRedisRepository(key string) repository.Repository {
-	return &redis.RedisStorage{}
+	v, _ := mr.connMap.Get(key)
+	st, _ := v.(repository.Repository)
+	return st
 }
